28d2cd9fb02718b22836047b5c1df56653f9d616
Mindrescu Marina <mindrescum2011c@gmail.com>
2022-09-11 00:25:09 +0300

    add pieces on table, add move for w-knight

---
+++ white.js
@@ -0,0 +1,32 @@
+[
+    {
+		"name": "Knight",
+		"length": 2,
+		"alias": "bn",
+		"position": { "letter": ["b", "g"], "number": 8 }
+	},
+	{
+		"name": "Bishope",
+		"length": 2,
+		"alias": "bb",
+		"position": { "letter": ["c", "f"], "number": 8 }
+	},
+	{
+		"name": "Rook",
+		"length": 2,
+		"alias": "br",
+		"position": { "letter": ["a", "h"], "number": 8 }
+	},
+	{
+		"name": "Queen",
+		"length": 1,
+		"alias": "bq",
+		"position": { "letter": ["d"], "number": 8 }
+	},
+	{
+		"name": "King",
+		"length": 1,
+		"alias": "bk",
+		"position": { "letter": ["e"], "number": 8 }
+	}
+]
